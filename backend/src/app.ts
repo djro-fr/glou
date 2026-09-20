fd2b648b748bd696cde83520db7ecee17a336c4a
Sylvain Girault <syl@djro.fr>
2026-09-20 18:16:13 +0200

    feat(backend): add dynamic filters to GET fountains

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,8 @@
 import '../config/env.ts'
 
 import express, { type Express, type Request, type Response } from 'express';
-import {client} from '../config/database.ts'
+import { client } from '../config/database.ts'
 import { transporter } from '../config/transporter.ts';
-import nodemailer from 'nodemailer';
 
 const app: Express = express();
 app.disable('x-powered-by');
@@ -14,9 +13,36 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/fountains', async(req: Request, res: Response) => {
+  
+  const filters = {
+    city: req.query.city,
+    district: req.query.district,
+    type: req.query.type,
+    status: req.query.status,
+  };
+  let conditions: string[] = [];
+  let values: string[] = [];
+  
+  for (const [key, value] of Object.entries(filters)) {
+    if (typeof value === 'string') {
+      values.push(value);
+      conditions.push(`${key} = $${values.length}`);
+    }
+  }
+    
+  let listConditions = "";
+  
+  for(let i=0; i < conditions.length; i++){    
+    if (i===0){
+      listConditions += ` WHERE ${conditions[i]}`;
+    } else {
+      listConditions += ` AND ${conditions[i]}`;
+    }     
+  }  
   const result = await client.query(
     `SELECT *
-     FROM fountain;`);
+     FROM fountain ${listConditions}`, 
+    values);
   res.send(result.rows);
 });
 
@@ -48,8 +74,6 @@ app.post('/contact', async(req: Request, res: Response) => {
     });
     res.json(req.body);
     console.log("Message sent: %s", info.messageId);
-    // Preview URL is only available when using an Ethereal test account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   } catch (err) {
     console.error("Error while sending mail:", err);
     res.status(500).json({error : "Erreur pendant l'envoi de l'e-mail"});
@@ -57,5 +81,5 @@ app.post('/contact', async(req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
