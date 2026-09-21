b5fc0a96af038d27e0d2b137830174ce41d6f1b7
Sylvain Girault <syl@djro.fr>
2026-09-21 18:50:25 +0200

    feat(frontend): fountain map

--- frontend/src/features/map/FountainMap.tsx
+++ frontend/src/features/map/FountainMap.tsx
@@ -1,9 +1,34 @@
-function FountainMap() {
+import {Map, setWorkerUrl} from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
 
+import { useEffect, useRef } from 'react';
+
+import './FountainMap.scss'
+
+setWorkerUrl(workerUrl);
+
+function FountainMap() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const map = useRef<Map | null>(null);
+  useEffect(() => {
+    if (!mapContainer.current) return;
+    const mapInstance = new Map({
+      container: mapContainer.current,
+      style: 'https://openmaptiles.geo.data.gouv.fr/styles/positron/style.json', // Positron style
+      center: [1.44, 43.60], // Toulouse
+      zoom: 12
+    });
+    map.current = mapInstance;
+    return () => {
+      // cleanup : détruire la carte à la démontée du composant
+      mapInstance.remove();
+    };
+  }, []);
   return (
     <>
-      <h1>Map</h1>     
-      <p>bonjour</p>
+      <h1>Trouvez une fontaine d’eau potable</h1>   
+      <div ref={mapContainer} id="map" className="map-container" /> 
     </>
   )
 }
--- frontend/src/shared/layout/Layout.tsx
+++ frontend/src/shared/layout/Layout.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router"
+import { Outlet, useLocation } from "react-router"
 
 import './Layout.scss'
 
 import Nav from "./Nav"
 
 function Layout() {
+  const location = useLocation();
+  const isMapPage = location.pathname === '/';
+  
   return (
     <>
       <Nav />
-      <main>
+      <main className={isMapPage ? 'map-page' : ''}>
         <Outlet />
       </main>
     </>
