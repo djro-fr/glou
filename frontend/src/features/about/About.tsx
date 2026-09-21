dbd4bc8de74b6dba35d0d1edb88354d9b253ee2c
Sylvain Girault <syl@djro.fr>
2026-09-21 17:10:06 +0200

    feat(frontend/about): template + style

--- frontend/src/features/about/About.tsx
+++ frontend/src/features/about/About.tsx
@@ -3,7 +3,19 @@ function About() {
   return (
     <>
       <h1>About</h1>     
-      <p>bonjour</p>
+      <div className="img-center">
+        <img width='150px' src="../../../images/Sylvain-Girault.png" alt="Sylvain Girault" />
+      </div>
+      <p>Je suis <strong>Sylvain GIRAULT</strong>, designer UX-UI et développeur fullstack web et applis mobiles, à Toulouse.</p>
+      <p>Depuis le début de ma vie professionnelle, je mélange créativité, design et passion de l'informatique pour optimiser l'expérience des utilisateurs, clients et partenaires.</p>
+      <p>Après un <strong>DUT Informatique</strong> puis une première carrière côté graphisme & webdesign pendant une vingtaine d’années, j'ai validé et renforcé mes compétences de Développeur Informatique grâce à une formation «&nbsp;<strong>Concepteur développeur d’applications</strong>&nbsp;», certifiée par un diplôme RNCP Niveau 6.</p>
+      <h2>Le projet GLOU</h2>
+      <div className="img-center">
+        <img width='90px' src="../../../images/logo-glou-couleur.png" alt="Logo GLOU" />
+      </div>
+      <p>Pour montrer mes compétences informatiques et mon savoir-faire, j’ai décidé de créer une application utile pour tous les habitant-e-s de Toulouse à partir de données ouvertes OpenData rassemblées par Toulouse Métropole sur leur site dédié.</p>
+      <p>On a bien vu l’importance des points d’eau et notamment des fontaines d’eau potable lors de la canicule de 2026. L’accessibilité de ces données me semble donc d’<strong>intérêt public</strong>.</p>
+      <p>J’ai ainsi créé un logo, un design system et une <strong>application web responsive, mobile-first</strong>. Pour connaître les spécifications techniques, je vous invite à découvrir le projet sur mon <a href="https://github.com/djro-fr" target="_blank">Github</a>.</p>
     </>
   )
 }
