f647e7077db0cff460409d0e0c7614771adcb254
Sylvain Girault <syl@djro.fr>
2026-09-22 18:12:32 +0200

    feat(backend): update Fountain repo, add district name with jointures

--- backend/src/Repository/fountainRepository.ts
+++ backend/src/Repository/fountainRepository.ts
@@ -2,16 +2,21 @@ import {client} from '../../config/database.ts'
 
 export async function getAllFountains(listConditions: string, values: string[]) {
   const result = await client.query(
-    `SELECT *
-    FROM fountain ${listConditions}`, 
+    `SELECT f.*, d.name_d
+    FROM fountain f 
+    JOIN district d 
+    ON f.district_number = d.number_d
+    ${listConditions};`, 
    values);
   return result.rows;
 }
 
 export async function getFountainById(id:number) {
   const result = await client.query(
-    `SELECT *
-     FROM fountain
+    `SELECT f.*, d.name_d
+     FROM fountain f     
+     JOIN district d 
+     ON f.district_number = d.number_d
      WHERE id =$1;`, [id]);
   return result.rows;
 }
